d7a5041b98fb6fff647656faec1f713f4f4e4037
NatawatOhm <natawat.cho@gmail.com>
2023-09-12 23:29:57 +0700

    create mainlayout with header and footer init

---
+++ src/components/mainLayout/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="global-padding w-full flex justify-between items-center h-[96px] border border-[#E5E5E5] font-[SukhumvitSet-SemiBold]">
+    <p class="text-[32px] text-[#5BBA47]">Interview</p>
+    <p class="text-[20px]">สั่งซื้อล่วงหน้า iPhone</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/components/mainLayout/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+    <slot name="content" />
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import Header from './Header.vue'
+  import Footer from './Footer.vue'
+</script>
+
+<style lang="scss" scoped></style>
